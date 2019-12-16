2902a2febbdb6c5f61d149c7073f0b6738675a47
Franklin Koch <franklin.koch@seequent.com>
2019-12-15 23:37:09 -0700

    2019.16 Fooling around with autotools

---
+++ 2019/16/src/fft.c
@@ -0,0 +1,9 @@
+// int signal_from_line(char *line, int *signal)
+// {
+//     int i = 0;
+//     while (line[i] != '\n') {
+//         signal[i] = line[i] - '0';
+//         i++;
+//     }
+//     return i;
+// }
---
+++ 2019/16/src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "fft.h"
+
+int main(int argc, char* argv[])
+{
+    // char* file_name = argv[1];
+    // char line[1024];
+    // fgets(line, sizeof(line), file);
+    // fclose(file);
+    // int signal[1024];
+    // int length = signal_from_line(line, signal);
+    printf("Ok\n");
+    return 0;
+}
+
+// int signal_from_line(char *line, int *signal)
+// {
+//     int i = 0;
+//     while (line[i] != '\n') {
+//         signal[i] = line[i] - '0';
+//         i++;
+//     }
+//     return i;
+// }
