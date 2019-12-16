6fe01896d78e786da1fd06d54fdb97bb158aedb4
Franklin Koch <franklin.koch@seequent.com>
2019-12-16 00:06:52 -0700

    2019.16 Implement signal_from_line

--- 2019/16/fft.c
+++ 2019/16/fft.c
@@ -2,11 +2,10 @@
 
 int signal_from_line(char *line, int *signal)
 {
-    return 1;
-    // int i = 0;
-    // while (line[i] != '\n') {
-    //     signal[i] = line[i] - '0';
-    //     i++;
-    // }
-    // return i;
+    int i = 0;
+    while (line[i] != '\n') {
+        signal[i] = line[i] - '0';
+        i++;
+    }
+    return i;
 }
