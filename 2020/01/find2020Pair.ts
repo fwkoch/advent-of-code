79938210d48c35ff6dd576e3be9b2fc668e8dd3a
Franklin Koch <franklin.koch@seequent.com>
2020-12-01 14:32:35 -0700

    2020.01 Find pair of numbers that add to 2020

---
+++ 2020/01/find2020Pair.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs';
+
+function readInput(filename: string): number[] {
+    let input: string = fs.readFileSync(filename, 'utf8');
+    let inputValues: string[] = input.split(/\r?\n/);
+    return inputValues.map(x => +x);
+}
+
+function findPair(values: number[], target: number = 2020): number {
+    let i: number;
+    let j: number;
+    for (i = 0; i < values.length; i++) {
+        for (j = i + 1; j < values.length; j++) {
+            if (values[i] + values[j] === target) {
+                return values[i] * values[j];
+            }
+        }
+    }
+    throw 'target not found';
+}
+
+console.log(findPair(readInput('input.txt')));
