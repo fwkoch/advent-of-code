9be53e2f492ba57aeab98768f232711371e97bfd
Franklin Koch <franklin.koch@seequent.com>
2020-12-01 14:36:09 -0700

    2020.01 Find trio of numbers that add to 2020

--- 2020/01/find2020Pair.ts
+++ 2020/01/find2020Pair.ts
@@ -19,4 +19,24 @@ function findPair(values: number[], target: number = 2020): number {
     throw 'target not found';
 }
 
-console.log(findPair(readInput('input.txt')));
+function findTrio(values: number[], target: number = 2020): number {
+    let i: number;
+    let j: number;
+    let k: number;
+    for (i = 0; i < values.length; i++) {
+        for (j = i + 1; j < values.length; j++) {
+            if (values[i] + values[j] >= target) {
+                continue
+            }
+            for (k = j + 1; k < values.length; k++) {
+                if (values[i] + values[j] + values[k] === target) {
+                    return values[i] * values[j] * values[k];
+                }
+            }
+        }
+    }
+    throw 'target not found';
+}
+
+// console.log(findPair(readInput('input.txt')));
+console.log(findTrio(readInput('input.txt')));
