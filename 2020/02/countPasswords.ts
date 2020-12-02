64dffaafefa2778ffdd753c2718fa7e6b0a169cb
Franklin Koch <franklin.koch@seequent.com>
2020-12-01 23:09:30 -0700

    2020.02 Count valid passwords a different way

--- 2020/02/countPasswords.ts
+++ 2020/02/countPasswords.ts
@@ -5,25 +5,34 @@ function readInput(filename: string): string[] {
   return input.split(/\r?\n/);
 }
 
-function passwordIsValid(password: string, char: string, min: number, max: number): boolean {
+function sledPasswordIsValid(password: string, char: string, min: number, max: number): boolean {
   const count: number = password.split(char).length - 1;
   return (count >= min && count <= max);
 }
 
-function validPasswordCount(inputs: string[]): number {
+function passwordIsValid(password: string, char: string, indexA: number, indexB: number): boolean {
+  const a: boolean = password.charAt(indexA - 1) === char;
+  const b: boolean = password.charAt(indexB - 1) === char;
+  return a !== b;
+}
+
+function validPasswordCount(
+  inputs: string[],
+  validator: (pw: string, char: string, a: number, b: number) => boolean,
+): number {
   let validCount: number = 0;
-  inputs.forEach((input: string) => {
-    console.log(input);
+  inputs.forEach((input: string): void => {
     const pieces: string[] = input.split(/[-: ]/);
     const min: number = +pieces[0];
     const max: number = +pieces[1];
     const char: string = pieces[2];
     const password: string = pieces[4];
-    if (passwordIsValid(password, char, min, max)) {
+    if (validator(password, char, min, max)) {
       validCount += 1;
     }
   });
   return validCount;
 }
 
-console.log(validPasswordCount(readInput('input.txt')));
+console.log(validPasswordCount(readInput('input.txt'), sledPasswordIsValid));
+console.log(validPasswordCount(readInput('input.txt'), passwordIsValid));
