ab1c38d375a71d28ffcb59570d1e375c9bae4374
Franklin Koch <franklin.koch@seequent.com>
2020-12-01 21:03:38 -0700

    Add some linting

--- 2020/01/find2020Pair.ts
+++ 2020/01/find2020Pair.ts
@@ -1,42 +1,41 @@
 import * as fs from 'fs';
 
 function readInput(filename: string): number[] {
-    let input: string = fs.readFileSync(filename, 'utf8');
-    let inputValues: string[] = input.split(/\r?\n/);
-    return inputValues.map(x => +x);
+  const input: string = fs.readFileSync(filename, 'utf8');
+  const inputValues: string[] = input.split(/\r?\n/);
+  return inputValues.map((x) => +x);
 }
 
 function findPair(values: number[], target: number = 2020): number {
-    let i: number;
-    let j: number;
-    for (i = 0; i < values.length; i++) {
-        for (j = i + 1; j < values.length; j++) {
-            if (values[i] + values[j] === target) {
-                return values[i] * values[j];
-            }
-        }
+  let i: number;
+  let j: number;
+  for (i = 0; i < values.length; i += 1) {
+    for (j = i + 1; j < values.length; j += 1) {
+      if (values[i] + values[j] === target) {
+        return values[i] * values[j];
+      }
     }
-    throw 'target not found';
+  }
+  throw new Error('target not found');
 }
 
 function findTrio(values: number[], target: number = 2020): number {
-    let i: number;
-    let j: number;
-    let k: number;
-    for (i = 0; i < values.length; i++) {
-        for (j = i + 1; j < values.length; j++) {
-            if (values[i] + values[j] >= target) {
-                continue
-            }
-            for (k = j + 1; k < values.length; k++) {
-                if (values[i] + values[j] + values[k] === target) {
-                    return values[i] * values[j] * values[k];
-                }
-            }
+  let i: number;
+  let j: number;
+  let k: number;
+  for (i = 0; i < values.length; i += 1) {
+    for (j = i + 1; j < values.length; j += 1) {
+      if (values[i] + values[j] < target) {
+        for (k = j + 1; k < values.length; k += 1) {
+          if (values[i] + values[j] + values[k] === target) {
+            return values[i] * values[j] * values[k];
+          }
         }
+      }
     }
-    throw 'target not found';
+  }
+  throw new Error('target not found');
 }
 
-// console.log(findPair(readInput('input.txt')));
+console.log(findPair(readInput('input.txt')));
 console.log(findTrio(readInput('input.txt')));
