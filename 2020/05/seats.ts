4e761ed7157ecddb935ebcf31547df711c568f38
Franklin Koch <franklin.koch@seequent.com>
2020-12-05 15:05:32 -0700

    2020.05 Update style

--- 2020/05/seats.ts
+++ 2020/05/seats.ts
@@ -6,7 +6,7 @@ function getSeats(filename: string): string[] {
 }
 
 function getValue(partition: string, upperChar: string): number {
-  let count: number = 2**(partition.length-1);
+  let count: number = 2 ** (partition.length - 1);
   let output: number = 0;
   let i: number;
   for (i = 0; i < partition.length; i += 1) {
@@ -27,37 +27,38 @@ function getCol(partition: string): number {
 }
 
 function getSeatID(partition: string): number {
-  return getRow(partition.substring(0, 7))*8 + getCol(partition.substring(7, 10))
+  return getRow(partition.substring(0, 7)) * 8 + getCol(partition.substring(7, 10));
 }
 
 function getMaxSeatID(partitions: string[]): number {
   let seatID: number = 0;
+  let id: number;
   partitions.forEach((partition: string): void => {
-    let id: number = getSeatID(partition);
+    id = getSeatID(partition);
     if (id > seatID) {
       seatID = id;
     }
-  })
+  });
   return seatID;
 }
 
 function getSortedSeatIDs(partitions: string[]): number[] {
-  let seatIDs: number[] = [];
+  const seatIDs: number[] = [];
   partitions.forEach((partition: string): void => {
     seatIDs.push(getSeatID(partition));
-  })
+  });
   return seatIDs.sort((a: number, b: number): number => a - b);
 }
 
 function findGapOfOne(sortedSeats: number[]): number {
   let i: number;
   for (i = 0; i < sortedSeats.length - 1; i += 1) {
-    if (sortedSeats[i+1] - sortedSeats[i] === 2) {
+    if (sortedSeats[i + 1] - sortedSeats[i] === 2) {
       return sortedSeats[i] + 1;
     }
   }
   throw new Error('seat not found');
 }
 
-console.log(getMaxSeatID(getSeats('input.txt')))
-console.log(findGapOfOne(getSortedSeatIDs(getSeats('input.txt'))))
+console.log(getMaxSeatID(getSeats('input.txt')));
+console.log(findGapOfOne(getSortedSeatIDs(getSeats('input.txt'))));
