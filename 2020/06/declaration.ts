a0ec395e3dda54d8fbc408372411380f3b3007fb
Franklin Koch <franklin.koch@seequent.com>
2020-12-05 22:56:23 -0700

    2020.05 Count all yesses - though this is pretty yucky

--- 2020/06/declaration.ts
+++ 2020/06/declaration.ts
@@ -14,4 +14,25 @@ function getAllYesses(declarations: string[]): number {
   return yesses;
 }
 
+function getAllDeclarations(filename: string): number {
+  const input: string = fs.readFileSync(filename, 'utf8');
+  const declarations: string[] = input.split('\n');
+  let count: number = 0;
+  let i: number;
+  let declarationSet: Set<string> = new Set(declarations[0]);
+  for (i = 1; i < declarations.length; i += 1) {
+    if (declarations[i] === '') {
+      count += declarationSet.size;
+      i += 1;
+      declarationSet = new Set(declarations[i]);
+    } else {
+      const comparisonSet: Set<string> = new Set(declarations[i]);
+      declarationSet = new Set([...declarationSet].filter((x: string) => comparisonSet.has(x)));
+    }
+  }
+  count += declarationSet.size;
+  return count;
+}
+
 console.log(getAllYesses(getDeclarations('input.txt')));
+console.log(getAllDeclarations('input.txt'));
