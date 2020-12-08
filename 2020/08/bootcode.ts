248ecb57e814c5361f373218edef3426caae20c8
Franklin Koch <franklin.koch@seequent.com>
2020-12-07 23:32:46 -0700

    2020.08 Fix bootcode - brute force seemed too easy, but ok!

--- 2020/08/bootcode.ts
+++ 2020/08/bootcode.ts
@@ -5,26 +5,52 @@ function getInstructions(filename: string): [string, number][] {
   return input.split('\n').map((instruction: string): [string, number] => [instruction.split(' ')[0], +instruction.split(' ')[1]]);
 }
 
-function runInstructions(instructions: [string, number][]): number {
+function runInstructions(instructions: [string, number][], modIndex?: number): number {
   let index: number = 0;
   let accumulator: number = 0;
   const visited: Set<number> = new Set();
-  while (true) {
+  let instruction: string;
+  let step: number;
+  while (index < instructions.length) {
     if (visited.has(index)) {
-      return accumulator;
+      throw new Error(`Loop found with accumulator ${accumulator}`);
     }
     visited.add(index);
-    if (instructions[index][0] === 'nop') {
+    [instruction, step] = instructions[index];
+    if (index === modIndex && instruction === 'jmp') {
+      instruction = 'nop';
+    } else if (index === modIndex && instruction === 'nop') {
+      instruction = 'jmp';
+    }
+    if (instruction === 'nop') {
       index += 1;
-    } else if (instructions[index][0] === 'acc') {
-      accumulator += instructions[index][1];
+    } else if (instruction === 'acc') {
+      accumulator += step;
       index += 1;
-    } else if (instructions[index][0] === 'jmp') {
-      index += instructions[index][1];
+    } else if (instruction === 'jmp') {
+      index += step;
     } else {
       throw new Error('unknown instruction');
     }
   }
+  return accumulator;
 }
 
-console.log(runInstructions(getInstructions('input.txt')));
+function findModifiedOutput(instructions: [string, number][]): number {
+  for (let i = 0; i < instructions.length; i += 1) {
+    if (instructions[i][0] !== 'acc') {
+      try {
+        return runInstructions(instructions, i);
+      // eslint-disable-next-line no-empty
+      } catch {}
+    }
+  }
+  throw new Error('no successful modification found');
+}
+
+try {
+  runInstructions(getInstructions('input.txt'));
+} catch (err) {
+  console.log(err);
+}
+console.log(findModifiedOutput(getInstructions('input.txt')));
