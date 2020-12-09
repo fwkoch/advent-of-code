c2ac2672d53b669c5fdc3683fcbe10c8940650c0
Franklin Koch <franklin.koch@seequent.com>
2020-12-08 22:55:09 -0700

    2020.09 Find encryption weakness

--- 2020/09/xmas.ts
+++ 2020/09/xmas.ts
@@ -24,4 +24,24 @@ function firstInvalid(entries: number[], preambleLength: number): number {
   throw new Error('all entries valid');
 }
 
-console.log(firstInvalid(getEntries('input.txt'), 25));
+function encryptionWeakness(entries: number[], target: number): number {
+  let sum: number;
+  let j: number;
+  for (let i: number = 0; i < entries.length - 1; i += 1) {
+    j = i + 1;
+    sum = entries[i];
+    while (sum < target) {
+      sum += entries[j];
+      j += 1;
+      if (sum === target) {
+        const slice: number[] = entries.slice(i, j).sort((a, b) => a - b);
+        return slice[0] + slice[slice.length - 1];
+      }
+    }
+  }
+  throw new Error('no contiguous sum found');
+}
+
+const entries: number[] = getEntries('input.txt');
+console.log(firstInvalid(entries, 25));
+console.log(encryptionWeakness(entries, firstInvalid(entries, 25)));
