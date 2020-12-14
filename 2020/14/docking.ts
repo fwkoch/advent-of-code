3fd2b66d4e5148e03fc058f53f64b69e1c8e5991
Franklin Koch <franklin.koch@seequent.com>
2020-12-14 00:45:15 -0700

    2020.14 Run docking program with rule 2

--- 2020/14/docking.ts
+++ 2020/14/docking.ts
@@ -48,4 +48,51 @@ function valueSum(memory: {[address: string]: number}): number {
   return sum;
 }
 
+function resolveFloaters(input: string): number[] {
+  let addrs: number[] = [];
+  if (input.includes('X')) {
+    addrs = addrs.concat(resolveFloaters(input.replace('X', '0')));
+    addrs = addrs.concat(resolveFloaters(input.replace('X', '1')));
+  } else {
+    addrs.push(parseInt(input, 2));
+  }
+  return addrs;
+}
+
+function applyMask(address: number, mask: string): string {
+  const output: string[] = [];
+  const addrString: string = address.toString(2).padStart(36, '0');
+  for (let i: number = 0; i < 36; i += 1) {
+    if (mask[i] === '0') {
+      output.push(addrString[i]);
+    } else {
+      output.push(mask[i]);
+    }
+  }
+  return output.join('');
+}
+
+function readInputRule2(input: string[]): {[address: string]: number} {
+  const memory: {[address: string]: number} = {};
+  let mask: string = '';
+  let address: string;
+  let value: string;
+  for (let i: number = 0; i < input.length; i += 1) {
+    if (input[i].startsWith('mask = ')) {
+      mask = input[i].slice(7);
+    } else {
+      if (mask === '') {
+        throw new Error('mask not defined yet.');
+      }
+      [, address, , , value] = input[i].split(/[ [\]]/);
+      const val: number = +value;
+      resolveFloaters(applyMask(+address, mask)).forEach((addr: number): void => {
+        memory[addr] = val;
+      });
+    }
+  }
+  return memory;
+}
+
 console.log(valueSum(readInput(getInput('input.txt'))));
+console.log(valueSum(readInputRule2(getInput('input.txt'))));
