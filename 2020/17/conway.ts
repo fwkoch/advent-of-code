e2c43fd1482e0a337764834318be5a0307ff238e
Franklin Koch <franklin.koch@seequent.com>
2020-12-16 23:50:08 -0700

    2020.17 Refactor to use linear index

--- 2020/17/conway.ts
+++ 2020/17/conway.ts
@@ -1,29 +1,44 @@
 import * as fs from 'fs';
 
-function getStart(filename: string): string[][][] {
+function getStart(filename: string): [string[], number[]] {
   const input: string[] = fs.readFileSync(filename, 'utf8').split('\n');
-  const output: string[][][] = [[]];
+  const output: string[] = [];
+  const dims: number[] = [input.length, input[0].length, 1];
   input.forEach((line: string): void => {
-    output[0].push(line.split(''));
+    line.split('').forEach((state: string): void => {
+      output.push(state);
+    });
   });
-  return output;
+  return [output, dims];
 }
 
-function status(input: string[][][], index: [number, number, number]) {
+function getLinearIndex(index: number[], dims: number[]): number {
+  let linearIndex: number = 0;
+  let dimProduct: number;
+  for (let i = 0; i < index.length; i += 1) {
+    dimProduct = dims.slice(i + 1).reduce((a: number, b: number): number => a * b, 1);
+    linearIndex += index[i] * dimProduct;
+  }
+  return linearIndex;
+}
+
+function status(input: string[], index: number[], dims: number[]) {
   const neighbors: string[] = [];
   let current: string = '.';
-  const ranges: [number, number][] = [
-    [Math.max(0, index[0] - 1), Math.min(input.length - 1, index[0] + 1)],
-    [Math.max(0, index[1] - 1), Math.min(input[0].length - 1, index[1] + 1)],
-    [Math.max(0, index[2] - 1), Math.min(input[0][0].length - 1, index[2] + 1)],
-  ];
+  const ranges: [number, number][] = [];
+  for (let i: number = 0; i < index.length; i += 1) {
+    ranges.push([
+      Math.max(0, index[i] - 1),
+      Math.min(dims[i] - 1, index[i] + 1),
+    ]);
+  }
   for (let i: number = ranges[0][0]; i <= ranges[0][1]; i += 1) {
     for (let j: number = ranges[1][0]; j <= ranges[1][1]; j += 1) {
       for (let k: number = ranges[2][0]; k <= ranges[2][1]; k += 1) {
         if (i !== index[0] || j !== index[1] || k !== index[2]) {
-          neighbors.push(input[i][j][k]);
+          neighbors.push(input[getLinearIndex([i, j, k], dims)]);
         } else {
-          current = input[i][j][k];
+          current = input[getLinearIndex([i, j, k], dims)];
         }
       }
     }
@@ -38,40 +53,35 @@ function status(input: string[][][], index: [number, number, number]) {
   return current;
 }
 
-function cycle(input: string[][][]): string[][][] {
-  const output: string[][][] = [];
-  let layer: string[][];
-  let row: string[];
-  for (let i: number = -1; i <= input.length; i += 1) {
-    layer = [];
-    for (let j: number = -1; j <= input[0].length; j += 1) {
-      row = [];
-      for (let k: number = -1; k <= input[0][0].length; k += 1) {
-        row.push(status(input, [i, j, k]));
+function cycle(input: string[], dims: number[]): string[] {
+  const output: string[] = [];
+  for (let i: number = -1; i <= dims[0]; i += 1) {
+    for (let j: number = -1; j <= dims[1]; j += 1) {
+      for (let k: number = -1; k <= dims[2]; k += 1) {
+        output[
+          getLinearIndex([i + 1, j + 1, k + 1], [dims[0] + 2, dims[1] + 2, dims[2] + 2])
+        ] = status(input, [i, j, k], dims);
       }
-      layer.push(row);
     }
-    output.push(layer);
   }
   return output;
 }
 
-function runCycles(numCycles: number, input: string[][][]): number {
-  let output: string[][][] = input;
+function runCycles(numCycles: number, input: string[], dims: number[]): number {
+  let output: string[] = input;
+  let outputDims: number[] = dims;
   for (let i = 0; i < numCycles; i += 1) {
-    output = cycle(output);
+    output = cycle(output, outputDims);
+    outputDims = outputDims.map((val: number): number => val + 2);
   }
   let numActive: number = 0;
-  output.forEach((layer: string[][]): void => {
-    layer.forEach((row: string[]): void => {
-      row.forEach((entry: string): void => {
-        if (entry === '#') {
-          numActive += 1;
-        }
-      });
-    });
+  output.forEach((entry: string): void => {
+    if (entry === '#') {
+      numActive += 1;
+    }
   });
   return numActive;
 }
 
-console.log(runCycles(6, getStart('input.txt')));
+const [output, dims]: [string[], number[]] = getStart('input.txt');
+console.log(runCycles(6, output, dims));
