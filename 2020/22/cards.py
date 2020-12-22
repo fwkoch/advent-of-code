48ae39b5956f2ae98fa1e22c8e0048a5946e763b
Franklin Koch <franklin.koch@seequent.com>
2020-12-21 22:42:11 -0700

    2020.22 Play some recursive combat

--- 2020/22/cards.py
+++ 2020/22/cards.py
@@ -15,16 +15,23 @@ class Hand:
         self.hand = self.hand[1:]
         return card
 
-    def win_cards(self, cards):
-        self.hand += sorted(cards)[::-1]
+    def win_cards(self, winning, losing):
+        self.hand += [winning, losing]
 
-    def play_round(self, opponent):
+    def play_round(self, opponent, recursive=False):
         mine = self.play_card()
         theirs = opponent.play_card()
-        if mine > theirs:
-            self.win_cards([mine, theirs])
+        if recursive and len(self.hand) >= mine and len(opponent.hand) >= theirs:
+            winner = self.play_game(opponent, True, [mine, theirs])
+            if winner.player == self.player:
+                self.win_cards(mine, theirs)
+            else:
+                opponent.win_cards(theirs, mine)
         else:
-            opponent.win_cards([mine, theirs])
+            if mine > theirs:
+                self.win_cards(mine, theirs)
+            else:
+                opponent.win_cards(theirs, mine)
 
     def has_lost(self):
         return len(self.hand) == 0
@@ -35,12 +42,24 @@ class Hand:
             score += card * (i + 1)
         return score
 
-    def play_game(self, opponent):
-        while not self.has_lost() and not opponent.has_lost():
-            self.play_round(opponent)
-        if self.has_lost():
-            return opponent.score()
-        return self.score()
+    def play_game(self, opponent, recursive=False, lengths=None):
+        if recursive and lengths:
+            player_0 = Hand(self.player, self.hand[: lengths[0]])
+            player_1 = Hand(opponent.player, opponent.hand[: lengths[1]])
+        else:
+            player_0 = self
+            player_1 = opponent
+        hands = set()
+        while not player_0.has_lost() and not player_1.has_lost():
+            if recursive:
+                hands_state = (tuple(player_0.hand), tuple(player_1.hand))
+                if hands_state in hands:
+                    return player_0
+                hands.add(hands_state)
+            player_0.play_round(player_1, recursive)
+        if player_0.has_lost():
+            return player_1
+        return player_0
 
 
 if __name__ == "__main__":
@@ -48,4 +67,7 @@ if __name__ == "__main__":
         hands = fid.read().split("\n\n")
     a = Hand.from_input(hands[0])
     b = Hand.from_input(hands[1])
-    print(a.play_game(b))
+    print(a.play_game(b).score())
+    a = Hand.from_input(hands[0])
+    b = Hand.from_input(hands[1])
+    print(a.play_game(b, recursive=True).score())
