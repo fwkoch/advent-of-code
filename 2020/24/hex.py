606f8af3e3ab13de508fb2c2373d39f7c4be6aae
Franklin Koch <franklin.koch@seequent.com>
2020-12-24 23:14:40 -0700

    2020.24 Reformat code

--- 2020/24/hex.py
+++ 2020/24/hex.py
@@ -1,34 +1,36 @@
 # locations are ~(e/w, n/s)
 
+
 def traverse(line):
     loc = [0, 0]
     i = 0
     while i < len(line):
         char = line[i]
         i += 1
-        if char == 'e':
+        if char == "e":
             loc[0] += 2
             continue
-        if char == 'w':
+        if char == "w":
             loc[0] -= 2
             continue
         char += line[i]
         i += 1
-        if char == 'se':
+        if char == "se":
             loc[0] += 1
             loc[1] -= 1
-        elif char == 'nw':
+        elif char == "nw":
             loc[0] -= 1
             loc[1] += 1
-        elif char == 'ne':
+        elif char == "ne":
             loc[0] += 1
             loc[1] += 1
-        elif char == 'sw':
+        elif char == "sw":
             loc[0] -= 1
             loc[1] -= 1
         else:
             raise ValueError(char)
     return tuple(loc)
+
 
 def flip(lines):
     black_tiles = set()
@@ -36,9 +38,11 @@ def flip(lines):
         black_tiles.symmetric_difference_update({traverse(line.strip())})
     return black_tiles
 
+
 def adjacent(tile):
     deltas = [[2, 0], [1, 1], [-1, 1], [-2, 0], [-1, -1], [1, -1]]
     return {(tile[0] + d[0], tile[1] + d[1]) for d in deltas}
+
 
 def daily_flip(tiles):
     new_tiles = set()
@@ -51,8 +55,9 @@ def daily_flip(tiles):
                 new_tiles.add(neighbor)
     return new_tiles
 
+
 if __name__ == "__main__":
-    with open('input.txt', 'r') as file:
+    with open("input.txt", "r") as file:
         tiles = flip(file.readlines())
     print(len(tiles))
     for _ in range(100):
