b62997e756f96dfb7a3bafefc9a92389612a278c
Franklin Koch <franklin.koch@seequent.com>
2021-12-02 12:20:05 -0700

    2021.02 Fix sub navigation

--- 2021/02/main.py
+++ 2021/02/main.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
+
 class Submarine(BaseModel):
     horiz: int = 0
     depth: int = 0
@@ -10,25 +11,54 @@ class Submarine(BaseModel):
     def instruction(self, command):
         action, value = command.split()
         value = int(value)
-        if action == 'forward':
-            self.horiz += value
-        elif action == 'down':
-            self.depth += value
-        elif action == 'up':
-            self.depth -= value
+        if action == "forward":
+            self.forward(value)
+        elif action == "down":
+            self.down(value)
+        elif action == "up":
+            self.up(value)
         else:
-            raise ValueError(f'bad action: {action}')
+            raise ValueError(f"bad action: {action}")
 
     def parse_instructions(self, filename):
-        with open(filename, 'r') as fid:
+        with open(filename, "r") as fid:
             for line in fid.readlines():
                 self.instruction(line)
 
+    def forward(self, value):
+        self.horiz += value
+
+    def down(self, value):
+        self.depth += value
+
+    def up(self, value):
+        self.depth -= value
+
+
+class ActualSubmarine(Submarine):
+    aim: int = 0
+
+    def forward(self, value):
+        self.horiz += value
+        self.depth += self.aim * value
+
+    def down(self, value):
+        self.aim += value
+
+    def up(self, value):
+        self.aim -= value
+
+
 if __name__ == "__main__":
     sub = Submarine()
-    sub.parse_instructions('./test_input.txt')
+    sub.parse_instructions("./test_input.txt")
     assert sub.mult() == 150
     sub = Submarine()
-    sub.parse_instructions('./input.txt')
+    sub.parse_instructions("./input.txt")
     print(sub.mult())
-
+    sub = ActualSubmarine()
+    sub.parse_instructions("./test_input.txt")
+    assert sub.mult() == 900
+    sub = ActualSubmarine()
+    sub.parse_instructions("./input.txt")
+    print(sub.mult())
