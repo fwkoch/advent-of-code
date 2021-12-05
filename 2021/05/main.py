df8ec50e62db23487688d92fc3e0d0763a45e10f
Franklin Koch <franklin.koch@seequent.com>
2021-12-04 23:42:03 -0700

    2021.05 Diagonals too

--- 2021/05/main.py
+++ 2021/05/main.py
@@ -21,19 +21,24 @@ class VentLine(BaseModel):
     def vertical(self):
         return self.start[0] == self.end[0]
 
-    def points(self):
-        if self.horizontal:
-            a = min(self.start[0], self.end[0])
-            b = max(self.start[0], self.end[0])
-            return [(i, self.start[1]) for i in range(a, b + 1)]
-        if self.vertical:
-            a = min(self.start[1], self.end[1])
-            b = max(self.start[1], self.end[1])
-            return [(self.start[0], i) for i in range(a, b + 1)]
-        return []
+    def points(self, diagonals=False):
+        if not (self.horizontal or self.vertical or diagonals):
+            return []
+        deltax = deltay = 0
+        if not self.horizontal:
+            deltay = (self.end[1] - self.start[1]) / abs(self.end[1] - self.start[1])
+        if not self.vertical:
+            deltax = (self.end[0] - self.start[0]) / abs(self.end[0] - self.start[0])
+        steps = (
+            abs(self.end[0] - self.start[0]) or abs(self.end[1] - self.start[1])
+        ) + 1
+        return [
+            (self.start[0] + deltax * i, self.start[1] + deltay * i)
+            for i in range(steps)
+        ]
 
-    def overlap(self, other):
-        return set(self.points()).intersection(other.points())
+    def overlap(self, other, diagonals=False):
+        return set(self.points(diagonals)).intersection(other.points(diagonals))
 
 
 def load_vents(filename):
@@ -44,11 +49,11 @@ def load_vents(filename):
     return lines
 
 
-def count_overlaps(lines):
+def count_overlaps(lines, diagonals=False):
     overlaps = set()
     for i in range(len(lines)):
         for other in lines[i + 1 :]:
-            overlaps = overlaps.union(lines[i].overlap(other))
+            overlaps = overlaps.union(lines[i].overlap(other, diagonals))
     return len(overlaps)
 
 
@@ -57,3 +62,5 @@ if __name__ == "__main__":
     assert count_overlaps(test_vents) == 5
     vents = load_vents("./input.txt")
     print(count_overlaps(vents))
+    assert count_overlaps(test_vents, diagonals=True) == 12
+    print(count_overlaps(vents, diagonals=True))
