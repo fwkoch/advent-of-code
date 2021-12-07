666461b85930fe9dfff63847ac3e570aea3f4081
Franklin Koch <franklin.koch@seequent.com>
2021-12-06 22:29:44 -0700

    2021.07 Nonlinear crab fuel

--- 2021/07/main.py
+++ 2021/07/main.py
@@ -3,14 +3,16 @@ def read_input(filename):
         return [int(val) for val in fid.readline().split(",")]
 
 
-def fuel(positions, final):
-    return sum(abs(position - final) for position in positions)
+def fuel(positions, final, linear=True):
+    if linear:
+        return sum(abs(position - final) for position in positions)
+    return sum(sum(range(abs(position - final) + 1)) for position in positions)
 
 
-def least_fuel(positions):
+def least_fuel(positions, linear=True):
     total = None
     for i in range(min(positions), max(positions)):
-        current_total = fuel(positions, i)
+        current_total = fuel(positions, i, linear)
         if total is None or current_total < total:
             total = current_total
     return total
@@ -21,3 +23,5 @@ if __name__ == "__main__":
     print(least_fuel(test_positions))
     positions = read_input("input.txt")
     print(least_fuel(positions))
+    print(least_fuel(test_positions, linear=False))
+    print(least_fuel(positions, linear=False))
