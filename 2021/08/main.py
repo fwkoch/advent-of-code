39c427f8b7dade0ce6fd75320913091db77d589d
Franklin Koch <franklin.koch@seequent.com>
2021-12-07 22:48:21 -0700

    2021.08 Figure out non-unique values

--- 2021/08/main.py
+++ 2021/08/main.py
@@ -20,12 +20,57 @@ class Display(BaseModel):
         instance.validate()
         return instance
 
+    def patterns_of_length(self, length):
+        return [val for val in self.patterns if len(val) == length]
+
+    def pattern_lookup(self):
+        lookup = {
+            1: self.patterns_of_length(2)[0],
+            7: self.patterns_of_length(3)[0],
+            4: self.patterns_of_length(4)[0],
+            8: self.patterns_of_length(7)[0],
+        }
+        for pattern in self.patterns_of_length(6):
+            if not all([val in pattern for val in lookup[1]]):
+                lookup[6] = pattern
+        for pattern in self.patterns_of_length(5):
+            if all([val in lookup[6] for val in pattern]):
+                lookup[5] = pattern
+        for pattern in self.patterns_of_length(5):
+            if pattern == lookup[5]:
+                continue
+            if sum([val in pattern for val in lookup[5]]) == 3:
+                lookup[2] = pattern
+            else:
+                lookup[3] = pattern
+        for pattern in self.patterns_of_length(6):
+            if pattern == lookup[6]:
+                continue
+            if all([val in pattern for val in lookup[5]]):
+                lookup[9] = pattern
+            else:
+                lookup[0] = pattern
+        return {val: key for key, val in lookup.items()}
+
+    def value(self):
+        lookup = self.pattern_lookup()
+        return (
+            lookup[self.output[0]] * 1000
+            + lookup[self.output[1]] * 100
+            + lookup[self.output[2]] * 10
+            + lookup[self.output[3]]
+        )
+
 
 def count_1478(displays):
     count = 0
     for display in displays:
         count += len([out for out in display.output if len(out) in [2, 3, 4, 7]])
     return count
+
+
+def count_values(displays):
+    return sum([display.value() for display in displays])
 
 
 def read_input(filename):
@@ -41,3 +86,5 @@ if __name__ == "__main__":
     assert count_1478(test_displays) == 26
     displays = read_input("input.txt")
     print(count_1478(displays))
+    assert count_values(test_displays) == 61229
+    print(count_values(displays))
