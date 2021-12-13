b39a06580d9ff18b31bde983214bad8858d1dc74
Franklin Koch <franklin.koch@seequent.com>
2021-12-13 01:21:58 -0700

    20211.13 Print fold output

--- 2021/13/main.py
+++ 2021/13/main.py
@@ -28,9 +28,25 @@ def read_input(filename):
     instructions = [(direction, int(axis)) for direction, axis in instructions]
     return dots, instructions
 
+def print_dots(dots):
+    xmax = max(dot[0] for dot in dots)
+    ymax = max(dot[1] for dot in dots)
+    grid = [['.' for _ in range(xmax + 1)] for _ in range(ymax + 1)]
+    for dot in dots:
+        grid[dot[1]][dot[0]] = '#'
+    for row in grid:
+        print(''.join(row))
+
+def carry_out_instructions(dots, instructions):
+    for instr in instructions:
+        dots = fold(dots, instr[0], instr[1])
+    print_dots(dots)
+
 
 if __name__ == "__main__":
     test_dots, test_instr = read_input("test_input.txt")
     assert len(fold(test_dots, test_instr[0][0], test_instr[0][1])) == 17
     dots, instr = read_input("input.txt")
     print(len(fold(dots, instr[0][0], instr[0][1])))
+    carry_out_instructions(test_dots, test_instr)
+    carry_out_instructions(dots, instr)
