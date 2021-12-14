4afca82cafb1e1dc9566c87942bde40cf4b42c5a
Franklin Koch <franklin.koch@seequent.com>
2021-12-14 00:15:01 -0700

    2021.14 Count elements efficiently without really polymerizing

--- 2021/14/main.py
+++ 2021/14/main.py
@@ -17,11 +17,32 @@ def polymerize(template, rules):
     return polymer
 
 
+def dont_really_polymerize(rules, pair_counts):
+    new_counts = {pair: 0 for pair in pair_counts}
+    for pair, count in pair_counts.items():
+        new_counts[pair[0] + rules[pair]] += count
+        new_counts[rules[pair] + pair[1]] += count
+    return new_counts
+
+
 def grow_polymer(template, rules, steps):
-    for i in range(steps):
+    for _ in range(steps):
         template = polymerize(template, rules)
     element_counts = [template.count(elem) for elem in set(template)]
     return max(element_counts) - min(element_counts)
+
+
+def dont_really_grow_polymer(template, rules, steps):
+    pair_counts = {pair: 0 for pair in rules}
+    for i in range(len(template) - 1):
+        pair_counts[template[i] + template[i + 1]] += 1
+    for _ in range(steps):
+        pair_counts = dont_really_polymerize(rules, pair_counts)
+    element_counts = {elem: 0 for elem in set("".join(rules))}
+    for pair, count in pair_counts.items():
+        element_counts[pair[0]] += count
+    element_counts[template[-1]] += 1
+    return max(element_counts.values()) - min(element_counts.values())
 
 
 if __name__ == "__main__":
@@ -29,3 +50,9 @@ if __name__ == "__main__":
     assert grow_polymer(test_template, test_rules, 10) == 1588
     template, rules = read_input("input.txt")
     print(grow_polymer(template, rules, 10))
+    assert dont_really_grow_polymer(test_template, test_rules, 10) == 1588
+    assert grow_polymer(template, rules, 10) == dont_really_grow_polymer(
+        template, rules, 10
+    )
+    assert dont_really_grow_polymer(test_template, test_rules, 40) == 2188189693529
+    print(dont_really_grow_polymer(template, rules, 40))
